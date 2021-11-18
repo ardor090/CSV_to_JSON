1cc68de7141a4ba5e58ed59bda0e9744bb64f106
eKow <ekowlloyd@gmail.com>
2021-11-18 18:56:01 +0100

    Update assess.py

--- assess.py
+++ assess.py
@@ -1,63 +1,62 @@
-import csv, json
+import csv
+import json
+ 
+ 
+# a function to import the data from csv - json)
+# takes two argument which eventually will be our csv source and json output.
+def convert_json(csvFile, jsonFile):
+ 
+    data = {}                                           # an empty dictionary that eventually will be populated 
+ 
+    with open(csvFile, 'r') as csvf:                    # this block of code imports the contents of the csv file into our program
+        csvReader = csv.DictReader(csvf)
+ 
+        for i, row in enumerate (csvReader):            # our loop that makes every key a number to eliminate the possibilty
+                                                        # that two identical last names gets overwritten
+            details = {
 
-file = 'acw_user_data.csv'
-jsonfilepath = 'sample.json'
+                
+                "first_name": row["First Name"],        # our pre-defined keys and structure of the dictionary
+                "second_name": row["Last Name"],
+                "age": row["Age (Years)"],
+                "sex": row["Sex"],
+                "retired": bool(row["Retired"]),
+                "marital_status": row["Marital Status"],
+                "dependents": row["Dependants"],
+                "salary": row["Yearly Salary (£)"],
+                "pension": row["Yearly Pension (£)"],
+                "company": row["Employer Company"],
+                "commute_distance": row["Distance Commuted to Work (miles)"],
+                "Address": {
+                    "street": row["Address Street"],
+                    "city": row["Address City"],
+                    "postcode": row["Address Postcode"],
+                },
+                "Credit Card": {
+                    "start_date": row["Credit Card Start Date"],
+                    "end_date": row["Credit Card Expiry Date"],
+                    "number": row["Credit Card Number"],
+                    "ccv": row["Credit Card CVV"],
+                    "iban": row["Bank IBAN"]
+                },
+                "Vehicle": {
+                    "make": row["Vehicle Make"],
+                    "model": row["Vehicle Model"],
+                    "year": row["Vehicle Year"],
+                    "category": row["Vehicle Type"]
 
-
-jsonArray = []
-with open(file, 'r') as f:
-    csvReader = csv.DictReader(f)
-    # next(reader)
-    # for val in csvReader:
-    #     print(val)
-    #     for k,v in list(val.items()):
-    #         if v == "" or v == " ":
-    #             del val[k]
-                # print(val)
-                # print(len(val))
-    for row in csvReader:
-    # for row in val:
-        details = {
-            "first_name": row["First Name"],
-            "second_name": row["Last Name"],
-            "age": row["Age (Years)"],
-            "sex": row["Sex"],
-            "retired": bool(row["Retired"]),
-            "marital_status": row["Marital Status"],
-            "dependents": row["Dependants"],
-            "salary": row["Yearly Salary (Â£)"],
-            "pension": row["Yearly Pension (Â£)"],
-            "company": row["Employer Company"],
-            "commute_distance": row["Distance Commuted to Work (miles)"],
-            "Address": {
-                "street": row["Address Street"],
-                "city": row["Address City"],
-                "postcode": row["Address Postcode"],
-            },
-            "Credit Card": {
-                "start_date": row["Credit Card Start Date"],
-                "end_date": row["Credit Card Expiry Date"],
-                "number": row["Credit Card Number"],
-                "ccv": row["Credit Card CVV"],
-                "iban": row["Bank IBAN"]
-            },
-            "Vehicle": {
-                "make": row["Vehicle Make"],
-                "model": row["Vehicle Model"],
-                "year": row["Vehicle Year"],
-                "category": row["Vehicle Type"]
+                }
 
             }
 
-        }
-
-        jsonArray.append(details)
-
-
-ab = json.dumps(jsonArray, indent=4, sort_keys=True)
-print(ab)
-
-# for data in jsonArray:
-#     for k, v in data.items():
-#         if v == "Lynne" and "last_name" == "Lynne":
-#             print(k,v)
+            data.setdefault(i, details)                      # returning the value of the key and assigning it
+    
+        
+    with open(jsonFile, 'w', encoding='utf-8') as jsonf:     # creating our json dict
+        jsonf.write(json.dumps(data, indent=4))
+    
+ 
+csvFile = r'acw_user_data (1).csv'
+jsonFile = r'Company_Db.json'
+ 
+convert_json(csvFile,jsonFile)                                # here we call the function with the two params
